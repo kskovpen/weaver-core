532ec2f463594893ba95943bc1694f3ebe82f73e
vsc47410 vscuser <vsc47410@gligar09.gastly.os>
2025-12-05 21:50:02 +0100

    update

---
+++ setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+export PYTHONPATH=/scratch/gent/474/vsc47410/pip/lib/python3.9/site-packages/:/scratch/gent/474/vsc47410/pip/lib64/python3.9/site-packages/:/scratch/gent/474/vsc47410/weaver-core
