d4cb522f097bbe3ea7afff6a31d4193b68814648
vsc47410 vscuser <vsc47410@gligar09.gastly.os>
2025-12-05 23:12:39 +0100

    update

--- runTrain.py
+++ runTrain.py
@@ -4,11 +4,11 @@ dataTrain="${VSC_DATA}/tree*.root"
 
 ${VSC_SCRATCH}/pip/bin/weaver --data-train ${dataTrain} \
  --data-test ${dataTrain} \
- --data-config lepmva/muon_binary.txt \
- --network-config networks/particlenet_pf_sv_withhighlevel.py \
+ --data-config ${VSC_SCRATCH}/weaver-core/lepmva/muon_binary.txt \
+ --network-config ${VSC_SCRATCH}/weaver-core/networks/particlenet_pf_sv_withhighlevel.py \
  --model-prefix model \
  --gpus 0,1,2,3 --batch-size 512 --start-lr 5e-3 --num-epochs 20 --optimizer ranger \
- --log logs/train.log
+ --log ${VSC_SCRATCH}/weaver-core/logs/train.log
 
 #~/.local/bin/weaver --data-train ${dataTrain} \
 # --data-test ${dataTrain} \
